a5cc4c1254f8d51157baee7852eaa54ad0b80e5c
patitapaban <patitapabanpanda046@gmail.com>
2024-05-24 11:52:54 +0530

    firebase

---
+++ firebase/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import ReactContact from "./components/reactContact";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <>
+      <ReactContact />
+    </>
+  );
+};
+
+export default App;
