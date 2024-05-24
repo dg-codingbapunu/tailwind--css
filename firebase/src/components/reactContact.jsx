34fd1d539c2685f8ea53ddf4f2e3488880fca969
patitapaban <patitapabanpanda046@gmail.com>
2024-05-24 12:23:46 +0530

    create a form

--- firebase/src/components/reactContact.jsx
+++ firebase/src/components/reactContact.jsx
@@ -3,41 +3,75 @@ import React from "react";
 const ReactContact = () => {
   return (
     <>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
+      <center className="contact">
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            Your Name
           </label>
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="exampleInputEmail1"
+            placeholder="Enter your Name"
             aria-describedby="emailHelp"
+            autoComplete="off"
           />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
-          </div>
+          <div id="emailHelp" className="form-text"></div>
         </div>
-        <div class="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Email Adress
           </label>
           <input
-            type="password"
-            class="form-control"
+            type="email"
+            className="form-control"
             id="exampleInputPassword1"
+            placeholder="Enter your Email password"
+            autoComplete="off"
           />
         </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
+        We'll never share your email with anyone else.
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Contact number
           </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Enter your phone number"
+            autoComplete="off"
+          />
         </div>
-        <button type="submit" class="btn btn-primary">
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Adress
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Enter your adress"
+            autoComplete="off"
+          />
+        </div>
+        <div className="mb-3">
+          <label for="exampleInputPassword1" className="form-label">
+            Message
+          </label>
+          <textarea
+            type="text"
+            rows="5"
+            className="form-control"
+            id="exampleInputPassword1"
+            autoComplete="off"
+          />
+        </div>
+        <div className="mb-3 form-check"></div>
+        <button type="submit" className="btn btn-primary">
           Submit
         </button>
-      </form>
+      </center>
     </>
   );
 };
